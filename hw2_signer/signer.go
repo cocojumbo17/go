9b580cb936dc757c2f87ac0c9794c60884ed2bab
cocojumbo17 <cocojumbo@ukr.net>
2021-10-10 22:14:52 +0300

    finish second test

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -26,47 +26,61 @@ func ExecutePipeline(jobs ...job) {
 }
 
 func SingleHash(in, out chan interface{}) {
+	wg_ex := sync.WaitGroup{}
 	for data := range in {
 		str := strconv.FormatInt(int64(data.(int)), 10)
-		var first, second string
-		wg := sync.WaitGroup{}
-		wg.Add(2)
-		go func() {
-			defer wg.Done()
-			first = DataSignerCrc32(str)
-		}()
-		go func() {
-			defer wg.Done()
-			second = DataSignerCrc32(DataSignerMd5(str))
-		}()
-		wg.Wait()
-		out <- first + "~" + second
+		md := DataSignerMd5(str)
+
+		wg_ex.Add(1)
+		go func(str, md string, out chan interface{}, wg_ex *sync.WaitGroup) {
+			defer wg_ex.Done()
+			var first, second string
+			wg := sync.WaitGroup{}
+			wg.Add(2)
+			go func() {
+				defer wg.Done()
+				first = DataSignerCrc32(str)
+			}()
+			go func() {
+				defer wg.Done()
+				second = DataSignerCrc32(md)
+			}()
+			wg.Wait()
+			out <- first + "~" + second
+		}(str, md, out, &wg_ex)
 	}
+	wg_ex.Wait()
 }
 
 func MultiHash(in, out chan interface{}) {
+	wg_ex := sync.WaitGroup{}
 	for data := range in {
-		var m map[int]string = make(map[int]string)
-		wgr := sync.WaitGroup{}
-		mux := sync.Mutex{}
-		wgr.Add(6)
-		for i := 0; i < 6; i++ {
-			go func(index int, data string, res map[int]string, wg *sync.WaitGroup, mx *sync.Mutex) {
-				defer wg.Done()
-				str := strconv.FormatInt(int64(index), 10)
-				s := DataSignerCrc32(str + data)
-				mx.Lock()
-				res[index] = s
-				mx.Unlock()
-			}(i, data.(string), m, &wgr, &mux)
-		}
-		wgr.Wait()
-		s := ""
-		for i := 0; i < 6; i++ {
-			s += m[i]
-		}
-		out <- s
+		wg_ex.Add(1)
+		go func(data string, out chan interface{}, wg_ex *sync.WaitGroup) {
+			defer wg_ex.Done()
+			var m map[int]string = make(map[int]string)
+			wgr := sync.WaitGroup{}
+			mux := sync.Mutex{}
+			wgr.Add(6)
+			for i := 0; i < 6; i++ {
+				go func(index int, data string, res map[int]string, wg *sync.WaitGroup, mx *sync.Mutex) {
+					defer wg.Done()
+					str := strconv.FormatInt(int64(index), 10)
+					s := DataSignerCrc32(str + data)
+					mx.Lock()
+					res[index] = s
+					mx.Unlock()
+				}(i, data, m, &wgr, &mux)
+			}
+			wgr.Wait()
+			s := ""
+			for i := 0; i < 6; i++ {
+				s += m[i]
+			}
+			out <- s
+		}(data.(string), out, &wg_ex)
 	}
+	wg_ex.Wait()
 }
 
 func CombineResults(in, out chan interface{}) {
